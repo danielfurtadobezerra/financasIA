87575609e760e923347a9b2269c471744b99cb64
Daniel <danielfurtadobezerra@gmail.com>
2026-03-03 18:55:18 +0000

    Upgrade to Android SDK 35 and Gradle 8.9

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.ia.financias"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ia.financias"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
