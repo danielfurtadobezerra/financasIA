d4e5a10ac353f1918eae44666b1e9565af10336a
Daniel <danielfurtadobezerra@gmail.com>
2026-03-03 18:46:28 +0000

    Correct Supabase dependency names

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -60,10 +60,10 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.5")
 
     // Supabase
-    implementation("io.github.jan-tennert.supabase:postgrest-kt:2.0.0")
-    implementation("io.github.jan-tennert.supabase:auth-kt:2.0.0")
-    implementation("io.github.jan-tennert.supabase:functions-kt:2.0.0")
-    implementation("io.github.jan-tennert.supabase:realtime-kt:2.0.0")
+    implementation("io.github.jan-tennert.supabase:postgrest:2.0.0")
+    implementation("io.github.jan-tennert.supabase:auth:2.0.0")
+    implementation("io.github.jan-tennert.supabase:functions:2.0.0")
+    implementation("io.github.jan-tennert.supabase:realtime:2.0.0")
     implementation("io.ktor:ktor-client-android:2.3.6")
 
     // Koin
