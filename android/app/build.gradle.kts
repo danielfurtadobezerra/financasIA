d96a0ad2b076cebdb1e9ccdfa03f40c7f7661d9c
Daniel <danielfurtadobezerra@gmail.com>
2026-03-03 20:01:20 +0000

    Major Upgrade: Kotlin 2.1.0, Compose Compiler 2.1.0, and Fix Icon References

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -38,10 +39,6 @@ android {
         compose = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
-    }
-
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -58,6 +55,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.compose.material:material-icons-extended")
 
     // Supabase
     implementation("io.github.jan-tennert.supabase:postgrest-kt:3.4.1")
@@ -73,7 +71,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.5.0")
 
     // Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
     // PDF Generation
     implementation("com.itextpdf:itext7-core:7.1.15")
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("com.android.application") version "8.8.2" apply false
     id("com.android.library") version "8.8.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
 
 tasks.register("clean", Delete::class) {
